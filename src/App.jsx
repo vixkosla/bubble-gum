97a6c17b298123b0c0c4024c72b7b5d39677774f
vixkosla <tomcool@yandex.ru>
2026-04-19 01:48:02 +0300

    fix: disable transmissionSampler on mobile to fix black spheres

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
 import { useControls } from 'leva'
 import './App.css'
 
+const IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
 function FPSStats() {
   const [fps, setFps] = useState(60)
   const [history, setHistory] = useState(() => Array(60).fill(60))
@@ -100,7 +102,7 @@ function BackgroundSphere() {
     <mesh position={[-1.15, 0.5, -10.3]}>
       <sphereGeometry args={[4.4, 28, 28]} />
       <MeshTransmissionMaterial
-        transmissionSampler
+        transmissionSampler={!IS_MOBILE}
         color={color}
         roughness={roughness}
         thickness={thickness}
@@ -409,7 +411,7 @@ function HypnoSphere({ position, size, glassControls = FRONT_GLASS_DEFAULTS, tin
       <sphereGeometry args={[size, res, res]} />
       <MeshTransmissionMaterial
         backside
-        transmissionSampler
+        transmissionSampler={!IS_MOBILE}
         color={tint}
         samples={1}
         resolution={48}
@@ -989,7 +991,7 @@ const AnimatedSphere = forwardRef(({ size, color, dCoef, index, transparent = tr
       <sphereGeometry args={[size, res, res]} />
       {useTransmissionMaterial ? (
         <MeshTransmissionMaterial
-          transmissionSampler
+          transmissionSampler={!IS_MOBILE}
           color={tint ?? glassControls.tint}
           roughness={glassControls.roughness}
           thickness={glassControls.thickness}
